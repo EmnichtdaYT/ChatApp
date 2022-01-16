34e7cdf08679a88b2fb698df8ad9d39961507c5c
EmnichtdaYT | Zoe <emnichtda@gmail.com>
2022-01-16 13:32:48 +0100

    formatting of test.js

--- test.js
+++ test.js
@@ -1,38 +1,38 @@
 app.get("/testdata", (req, res, next) => {
-    console.log("TEST DATA :");
-    pool.query("Select * from test").then((testData) => {
-      console.log(testData);
-      res.send(testData.rows);
-    });
+  console.log("TEST DATA :");
+  pool.query("Select * from test").then((testData) => {
+    console.log(testData);
+    res.send(testData.rows);
   });
-  
-  app.post("/testdata", (req, res, next) => {
-    console.log("TEST DATA POST");
-    req.body;
-    pool.query("INSERT INTO test (id) VALUES (" + req.body.data + ")");
-    console.log(req.body.data);
-    res.json(JSON.parse('{"status":"success"}'));
+});
+
+app.post("/testdata", (req, res, next) => {
+  console.log("TEST DATA POST");
+  req.body;
+  pool.query("INSERT INTO test (id) VALUES (" + req.body.data + ")");
+  console.log(req.body.data);
+  res.json(JSON.parse('{"status":"success"}'));
+});
+
+app.get("/generalchat", (req, res, next) => {
+  console.log("GENERALCHAT:");
+  pool.query("Select * from generalchat").then((testData) => {
+    console.log(testData);
+    res.send(testData.rows);
   });
-  
-  app.get("/generalchat", (req, res, next) => {
-    console.log("GENERALCHAT:");
-    pool.query("Select * from generalchat").then((testData) => {
-      console.log(testData);
-      res.send(testData.rows);
-    });
-  });
-  
-  app.post("/test", (req, res, next) => {
-    console.log("test");
-    res.send(req.body);
-  });
-  
-  app.post("/generalchat", (req, res, next) => {
-    console.log("GENERALCHAT POST");
-    req.body;
-    pool.query(
-      "INSERT INTO generalchat (message) VALUES ('" + req.body.data + "')"
-    );
-    console.log(req.body.data);
-    res.json({ status: "success" });
-  });
+});
+
+app.post("/test", (req, res, next) => {
+  console.log("test");
+  res.send(req.body);
+});
+
+app.post("/generalchat", (req, res, next) => {
+  console.log("GENERALCHAT POST");
+  req.body;
+  pool.query(
+    "INSERT INTO generalchat (message) VALUES ('" + req.body.data + "')"
+  );
+  console.log(req.body.data);
+  res.json({ status: "success" });
+});
