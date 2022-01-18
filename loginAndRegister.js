d4d119c63fc23cab93d62922a27688d99623a18a
EmnichtdaYT | Zoe <emnichtda@gmail.com>
2022-01-18 15:39:04 +0100

    habe probiert mysql statt postgre zu verwenden hat aber nicht geklappt außerdem ! zu token hinzugefügt

--- loginAndRegister.js
+++ loginAndRegister.js
@@ -68,7 +68,7 @@ async function isPasswordCorrect(username, password) {
 function generateRandomString() {
   var result = "";
   var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!";
   var charactersLength = characters.length;
   for (var i = 0; i < 20; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
