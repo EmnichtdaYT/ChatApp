5ac206d44919737189b2d40581eb45927e71988a
EmnichtdaYT | Zoe <emnichtda@gmail.com>
2022-02-18 10:24:43 +0100

    implemented ability to send messages to chats

--- loginAndRegister.js
+++ loginAndRegister.js
@@ -76,6 +76,34 @@ function initLoginAndRegister(app, database) {
     res.json({tokenCorrect: true, messages: re.rows})
 
   })
+
+  app.post("/:token/:chatid", async (req, res, next) => {
+    var token = req.params.token;
+    var tokenCorrect = token in userTokens;
+    var chatid = req.params.chatid
+    var message = req.body.message;
+
+    if(!tokenCorrect){
+      res.json({ tokenCorrect: false });
+      return;
+    }
+
+    if(!await hasUserPermissionForChat(userTokens[token], chatid)){
+      res.json({ hasPermission: false });
+      return;
+    }
+
+    if(!message){
+      res.json({ error: "message is not defined" })
+      return;
+    }
+
+    var messageid = await generateMessageIdForChat(chatid);
+
+    db.query("INSERT INTO chatmessages (chatid, messageid, sentby, message) VALUES ($1, $2, $3, $4)", [chatid, messageid, userTokens[token], message])
+
+    res.json({ messageid: messageid });
+  })
 }
 
 var userTokens = {};
@@ -134,6 +162,16 @@ async function hasUserPermissionForChat(user, chatid){
   var result = await db.query("SELECT COUNT(*) FROM chatpermissions WHERE chatid = $1 AND haspermission = $2", [chatid, user]);
 
   return result.rows[0].count != 0;
+}
+
+async function generateMessageIdForChat(chatid){
+  var messageid = generateRandomString()
+  var result = await db.query("SELECT COUNT(*) FROM chatmessages WHERE chatid = $1 AND messageid = $2", [chatid, messageid]);
+  if(result.rows[0].count == 0){
+    return messageid;
+  }else{
+    return generateMessageIdForChat(chatid);
+  }
 }
 
 module.exports = {
